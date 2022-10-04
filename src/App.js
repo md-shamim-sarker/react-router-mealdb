df71ca888c054d923632dd26a06cd8eb6385b138
shamimspro <shamimspro@gmail.com>
2022-10-04 10:18:54 +0600

    create router and routerprovider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './App.css';
 
 const App = () => {
+  const router = createBrowserRouter([
+    {path: "/", element: <h2>Home Page</h2>},
+    {path: "/home", element: <h2>Home Page</h2>},
+    {path: "/about", element: <h2>About Page</h2>},
+    {path: "/service", element: <h2>Service Page</h2>},
+  ]);
   return (
     <div className='App'>
-      <h2>Hello World!</h2>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 };
